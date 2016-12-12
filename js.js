60a0bffb9bbfb10c735f1ef73d4914410d39b708
Thomas <ktssta@gmail.com>
2016-12-12 22:24:53 +0100

    update

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-var bs = ["360 No Scope", "420 Blaze It", "322 Solo", "60 Frames Per Second"];
-var rdm = (Math.ceil(Math.random(bs) * 4) - 1);
+var bs = ["360 No Scope", "420 Blaze It", "322 Solo Mid", "60 Frames Per Second", "700 更新 新游记"];
+var rdm = (Math.ceil(Math.random(bs) * 5) - 1);
 console.log(rdm);
 console.log(bs[rdm]);
