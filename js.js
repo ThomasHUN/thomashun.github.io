9a21c8d081dc22357d3e9ef263ea01c25aeb5ad0
Thomas <ktssta@gmail.com>
2016-12-12 21:52:26 +0100

    update

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-var bs = ["", "360 No Scope", "420 Blaze It", "322 Solo", "60 Frames Per Second"];
-var rdm = Math.ceil(Math.random(bs) * 4);
+var bs = ["360 No Scope", "420 Blaze It", "322 Solo", "60 Frames Per Second"];
+var rdm = (Math.ceil(Math.random(bs) * 5) - 1);
 console.log(rdm);
 console.log(bs[rdm]);
