39242d0e7a1737572c30eacdf319ff1c95be11ab
Thomas <ktssta@gmail.com>
2016-12-12 21:38:37 +0100

    u

--- js.js
+++ js.js
@@ -1,5 +1,3 @@
 var bs = ["360 No Scope","420 Blaze It","322 Solo","60 Frames Per Second"];
-
-var rbs = Math.random(bs);
-
-console.log(rbs[0]);
+var rdm = Math.ceil(Math.random(bs) * 4);
+console.log(bs[rdm]);
